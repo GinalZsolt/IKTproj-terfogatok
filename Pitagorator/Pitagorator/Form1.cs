3365e849c429538ddc897b77ab49308a54fbd59c
DavidKocsis0204 <kocsisdavid20020204@gmail.com>
2022-11-09 12:29:48 +0100

    welcome kész

--- Pitagorator/Pitagorator/Form1.cs
+++ Pitagorator/Pitagorator/Form1.cs
@@ -48,8 +48,79 @@ namespace Pitagorator
             for (int i = 0; i < alakzatnevek.Count; i++) welc_Cbox.Items.Add(alakzatnevek[i]);
             welc_Cbox.Text = "Háromszög";
             welc_Cbox.DropDownStyle = ComboBoxStyle.DropDownList;
+            welc_Cbox.SelectedIndexChanged += welc_CboxChange;
+            p_box = new PictureBox()
+            {
+                Top = welc_Cbox.Top + 25,
+                Height = 200,
+                Width = 200,
+                Left = (this.Width / 2) - 100,
+                Image = Properties.Resources.haromszog,
+                SizeMode = PictureBoxSizeMode.StretchImage
+            };
+            start_btn.Text = "Számol";
+            start_btn.Top = p_box.Top + p_box.Height + 35;
+            start_btn.Left = this.Width / 2 - 40;
+            start_btn.Height = 20;
+            start_btn.Width = 80;
+            label1.Text = "Háromszög";
+            label1.Visible = false;
+            this.Controls.Add(start_btn);
+            this.Controls.Add(p_box);
             this.Controls.Add(welc_Cbox);
-            
+        }
+
+
+        private void welc_CboxChange(object sender, EventArgs e)
+        {
+            label1.Visible = true;
+            label1.Text = (sender as ComboBox).SelectedItem.ToString();
+            label1.Visible = false;
+            switch ((sender as ComboBox).SelectedItem.ToString())
+            {
+                case "Háromszög":
+                    p_box.Image = Properties.Resources.haromszog;
+                    break;
+                case "Henger":
+                    p_box.Image = Properties.Resources.henger;
+                    break;
+                case "Paralelogramma":
+                    p_box.Image = Properties.Resources.paralelogramma;
+                    break;
+                case "Négyzet":
+                    p_box.Image = Properties.Resources.negyzet;
+                    break;
+                case "Deltoid":
+                    p_box.Image = Properties.Resources.deltoid;
+                    break;
+                case "Gömb":
+                    p_box.Image = Properties.Resources.gomb;
+                    break;
+                case "Gúla":
+                    p_box.Image = Properties.Resources.gula;
+                    break;
+                case "Trapéz":
+                    p_box.Image = Properties.Resources.hurtrapez;
+                    break;
+                case "Kör":
+                    p_box.Image = Properties.Resources.kor;
+                    break;
+                case "Rombusz":
+                    p_box.Image = Properties.Resources.rombusz;
+                    break;
+                case "Téglalap":
+                    p_box.Image = Properties.Resources.teglalap;
+                    break;
+            }
+        }
+
+        private void start_btn_Click(object sender, EventArgs e)
+        {
+            label1.Visible = true;
+            Szamolo szamolo = new Szamolo(label1.Text);
+            szamolo.Show();
+            label1.Visible = false;
+            this.Hide();
         }
     }
 }
---
+++ Pitagorator/Pitagorator/Szamolo.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Pitagorator
+{
+    public partial class Szamolo : Form
+    {
+        private string Alakzat { get; set; }
+
+        public Szamolo(string alakzat)
+        {
+            InitializeComponent();
+            this.Text = $"{alakzat} számoló";
+            this.Alakzat = alakzat;
+        }
+
+        private void Szamolo_Load(object sender, EventArgs e)
+        {
+            this.FormBorderStyle = FormBorderStyle.FixedSingle;
+            p_box = new PictureBox()
+            {
+                Top = 60,
+                Height = 250,
+                Width = 250,
+                Left = (this.Width / 2) + 50,
+                SizeMode = PictureBoxSizeMode.StretchImage
+            };
+            this.Controls.Add(p_box);
+            kepEldontes();
+        }
+
+        private void kepEldontes()
+        {
+            switch (Alakzat)
+            {
+                case "Háromszög":
+                    p_box.Image = Properties.Resources.adat_haromszog;
+                    break;
+                case "Henger":
+                    p_box.Image = Properties.Resources.adat_henger;
+                    break;
+                case "Paralelogramma":
+                    p_box.Image = Properties.Resources.adat_paralelogramma;
+                    break;
+                case "Négyzet":
+                    p_box.Image = Properties.Resources.adat_negyzet;
+                    break;
+                case "Deltoid":
+                    p_box.Image = Properties.Resources.adat_deltoid;
+                    break;
+                case "Gömb":
+                    p_box.Image = Properties.Resources.adat_gomb;
+                    break;
+                case "Gúla":
+                    p_box.Image = Properties.Resources.adat_gula;
+                    break;
+                case "Trapéz":
+                    p_box.Image = Properties.Resources.adat_hurtrapez;
+                    break;
+                case "Kör":
+                    p_box.Image = Properties.Resources.adat_kor;
+                    break;
+                case "Rombusz":
+                    p_box.Image = Properties.Resources.rombusz;
+                    break;
+                case "Téglalap":
+                    p_box.Image = Properties.Resources.teglalap;
+                    break;
+            }
+        }
+    }
+}
