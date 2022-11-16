1450b10a9b9bf3bc75d0b2aea1cc97c9ab272906
DavidKocsis0204 <kocsisdavid20020204@gmail.com>
2022-11-16 08:17:45 +0100

    különböző formok designjának ki szervezése saját osztályba

---
+++ Pitagorator/Pitagorator/szamolo_form_elements_des.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Pitagorator
+{
+    class szamolo_form_elements_des
+    {
+
+    }
+}
